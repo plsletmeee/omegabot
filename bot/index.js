05344566e0750caa79ec28038d3792f1c6448709
Charlie S <100416544+Xictul@users.noreply.github.com>
2022-11-11 23:41:45 +0000

    Enabled Discord RPC and added comments

--- bot/index.js
+++ bot/index.js
@@ -1,5 +1,12 @@
-require('events').defaultMaxListeners = 40;
+// -- Dev Notice --
+// Please do not add anything here without permission from Xictul as any changes could break the bot.
 
+
+// Welcome to the Omega Bot Index file. This is the main file for the discord bot. All other files, relating to the bot itself, are activated from here.
+
+require('events').defaultMaxListeners = 40; // see this for info on event listeners: https://nodejs.org/api/events.html
+
+// this is for setting up the bot clients
 const { Client, Collection, Partials } = require('discord.js');
 const client = new Client({intents: 3276799}, { partials: [Partials.Message, Partials.Channel, Partials.Reaction] });
 const premium = new Client({intents: 3276799}, { partials: [Partials.Message, Partials.Channel, Partials.Reaction] });
@@ -7,6 +14,7 @@ const premium = new Client({intents: 3276799}, { partials: [Partials.Message, Pa
 module.exports = client;
 module.exports = premium;
 
+// this is for setting up the distube package
 const { DisTube } = require('distube')
 const { YtDlpPlugin } = require('@distube/yt-dlp')
 
@@ -26,14 +34,17 @@ premium.distube = new DisTube(premium, {
     ],
 });
 
+// this is adding the global variables for use in other files
 client.commands = new Collection();
 premium.commands = new Collection();
 premium.enabled = false;
 
-require('./handlers/mongodb')(client, premium);
-require('./handlers/commands')(client, premium);
-require('./handlers/events')(client, premium);
+require('../rpc/index') // loads the discord-rpc statuses
+
+require('./handlers/mongodb')(client, premium); // loads the database
+require('./handlers/commands')(client, premium); // loads the bot commands
+require('./handlers/events')(client, premium); // loads the bot events
 require('dotenv').config();
 
-client.login(process.env.MAIN_BOT_TOKEN || process.env.TEST_BOT_TOKEN);
-if(premium.enabled !== false) premium.login(process.env.PREMIUM_BOT_TOKEN || process.env.TEST_BOT_TOKEN);
+client.login(process.env.MAIN_BOT_TOKEN || process.env.TEST_BOT_TOKEN); // logs in the main or testing bot (main bot token stored on bot server)
+if(premium.enabled) premium.login(process.env.PREMIUM_BOT_TOKEN); // logs in the premium bot (when allowed to)
