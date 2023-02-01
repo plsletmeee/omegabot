55126f614f2b41308aef12ee9ad80a9a162d8d9d
Xictul <saunderscharlie@hotmail.com>
2023-02-01 01:08:23 +0000

    Update 2.2.1

--- bot/handlers/commands.js
+++ bot/handlers/commands.js
@@ -41,12 +41,12 @@ module.exports = async (client) => {
             // Pushing New Commands (Dev)
             client.guilds.cache.forEach(guild => {
                 guild.commands.set([])
-                guild.commands.set(mainCmd)
+                // guild.commands.set(mainCmd)
             })
 
             // Pushing New Commands (Prod)
-            // client.application.commands.set([])
-            // client.application.commands.set(mainCmd)
+            client.application.commands.set([])
+            client.application.commands.set(mainCmd)
 
             // Updating Bot Statistics
             const botStatistics = await require('../../database/botStatistics').findById('63c4559e6b831d6faa89d5d7')
