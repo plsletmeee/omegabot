e2c345018076c9d24dc49e9580cf1e5917d66fd9
xictul <100416544+xictul@users.noreply.github.com>
2023-04-02 20:01:00 +0100

    Update commands.js

--- bot/handlers/commands.js
+++ bot/handlers/commands.js
@@ -39,14 +39,14 @@ module.exports = async (client) => {
 
         client.on('ready', async () => {
             // Pushing New Commands (Dev)
-            client.guilds.cache.forEach(guild => {
-                guild.commands.set([])
-                guild.commands.set(mainCmd)
-            })
+//             client.guilds.cache.forEach(guild => {
+//                 guild.commands.set([])
+//                 guild.commands.set(mainCmd)
+//             })
 
             // Pushing New Commands (Prod)
             client.application.commands.set([])
-            // client.application.commands.set(mainCmd)
+            client.application.commands.set(mainCmd)
 
             // Updating Bot Statistics
             if(client.user.id != '1046548033552273469') {
